f2028e1d4a9d568cc7b75ed6ad0020d04ebf6aae
hemanthtpg <45250190+pedamallu@users.noreply.github.com>
2022-03-26 14:45:52 +0530

    Added user authentication & authorization

--- src/main/java/com/onlineplantnursery/service/PlanterServiceImpl.java
+++ src/main/java/com/onlineplantnursery/service/PlanterServiceImpl.java
@@ -1,5 +1,6 @@
 package com.onlineplantnursery.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,23 @@ public class PlanterServiceImpl implements IPlanterService {
 
 	@Override
 	public Planter addPlanter(Planter planter) {
-		
+//		Optional<EndUser> endUser = endUserRepo.findById(id);
+//		if(!endUser.isPresent())
+//		{
+//			throw new GardenDecorException("admin not found with the given id:" + id);
+//		}
+//		else if(!endUser.get().getRole().equals("admin"))
+//		{
+//			throw new GardenDecorException("only admin can add planter to the database");
+//		}
+//		else if(!endUser.get().getLogin().isLogin())
+//		{
+//			throw new GardenDecorException("first login to add planter to the database");
+//		}
+//		else
+//		{
 		return planterRepo.save(planter);
+//		}
 	}
 
 //	@Override
@@ -37,25 +53,108 @@ public class PlanterServiceImpl implements IPlanterService {
 
 	@Override
 	public Planter getPlanterByName(String name) {
-		
+//		Optional<EndUser> endUser = endUserRepo.findById(id);
+//		if(!endUser.isPresent())
+//		{
+//			throw new GardenDecorException("admin not found with the given id:" + id);
+//		}
+//		else if(!endUser.get().getRole().equals("admin"))
+//		{
+//			throw new GardenDecorException("only admin can add planter to the database");
+//		}
+//		else if(!endUser.get().getLogin().isLogin())
+//		{
+//			throw new GardenDecorException("first login to add planter to the database");
+//		}
+//		else
+//		{
 		return planterRepo.getByName(name);
+//		}
 	}
 
 	@Override
 	public Planter deletePlanterById(int id) {
+//		Optional<EndUser> endUser = endUserRepo.findById(id);
+//		if(!endUser.isPresent())
+//		{
+//			throw new GardenDecorException("admin not found with the given id:" + id);
+//		}
+//		else if(!endUser.get().getRole().equals("admin"))
+//		{
+//			throw new GardenDecorException("only admin can add planter to the database");
+//		}
+//		else if(!endUser.get().getLogin().isLogin())
+//		{
+//			throw new GardenDecorException("first login to add planter to the database");
+//		}
+//		else
+//		{
 		// Fetch the existing record from database
-		Planter planter=planterRepo.getById(id);
+		Optional<Planter> planter=planterRepo.findById(id);
+		if(!planter.isPresent())
+			throw new PlanterNotFoundException("Planter not found with id: "+id);
 		//Use JPARepository method
-		planterRepo.delete(planter);
-		return planter;
+		planterRepo.delete(planter.get());
+		return planter.get();
+//		}
 	}
 
 	@Override
 	public Planter updatePlanterPrice(int id, float price) {
-		
-		Planter planter=planterRepo.getById(id);
-		planter.setPrice(price);
-		return planterRepo.save(planter);
+//		Optional<EndUser> endUser = endUserRepo.findById(id);
+//		if(!endUser.isPresent())
+//		{
+//			throw new GardenDecorException("admin not found with the given id:" + id);
+//		}
+//		else if(!endUser.get().getRole().equals("admin"))
+//		{
+//			throw new GardenDecorException("only admin can add planter to the database");
+//		}
+//		else if(!endUser.get().getLogin().isLogin())
+//		{
+//			throw new GardenDecorException("first login to add planter to the database");
+//		}
+//		else
+//		{
+			Optional<Planter> planter=planterRepo.findById(id);
+			if(!planter.isPresent())
+				throw new PlanterNotFoundException("Planter not found with id: "+id);
+			Planter updatedPlanter=planter.get();
+			updatedPlanter.setPrice(price);
+			return planterRepo.save(updatedPlanter);
+//		}
+	}
+
+	@Override
+	public List<Planter> getAllPlanters() {
+		// Fetches all the planter records from DB
+		return planterRepo.findAll();
+	}
+
+	@Override
+	public Planter updatePlanterPhoto(int planterId,String photoLoc) {
+//		Optional<EndUser> endUser = endUserRepo.findById(id);
+//		if(!endUser.isPresent())
+//		{
+//			throw new GardenDecorException("admin not found with the given id:" + id);
+//		}
+//		else if(!endUser.get().getRole().equals("admin"))
+//		{
+//			throw new GardenDecorException("only admin can add planter to the database");
+//		}
+//		else if(!endUser.get().getLogin().isLogin())
+//		{
+//			throw new GardenDecorException("first login to add planter to the database");
+//		}
+//		else
+//		{
+			Optional<Planter> planter=planterRepo.findById(planterId);
+			if(!planter.isPresent())
+				throw new PlanterNotFoundException("Planter not found with id: "+planterId);
+			Planter updatedPlanter=planter.get();
+			updatedPlanter.setPhotoLoc(photoLoc);
+			return planterRepo.save(updatedPlanter);
+//		}
 	}
 	
 	
